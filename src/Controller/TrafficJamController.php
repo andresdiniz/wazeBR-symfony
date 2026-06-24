69a55611f18640d902a6f81cd7879841e08bbeec
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-06-24 15:32:42 -0300

    fix(routes): corrige prefixo da rota TrafficJamController de jam_ para traffic_jam_

--- src/Controller/TrafficJamController.php
+++ src/Controller/TrafficJamController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/congestionamentos', name: 'jam_')]
+#[Route('/congestionamentos', name: 'traffic_jam_')]
 #[IsGranted('ROLE_USER')]
 class TrafficJamController extends AbstractController
 {
