de8bcd57818751add55f7eb3e782bd4fc19e0266
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-08-07 12:15:03 -0300

    fix: remover updated_at da entidade User para alinhar com tabela users existente
    - Remove campo updated_at que não existe atualmente no banco
    - Mantém createdAt para referência de criação
    - Evita InvalidFieldNameException em SELECT de login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,13 +41,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $updatedAt = null;
-
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
-        $this->updatedAt = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -132,22 +128,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $this->createdAt = $createdAt;
         return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
-    {
-        $this->updatedAt = $updatedAt;
-        return $this;
-    }
-
-    #[ORM\PreUpdate]
-    public function onPreUpdate(): void
-    {
-        $this->updatedAt = new \DateTimeImmutable();
     }
 }
