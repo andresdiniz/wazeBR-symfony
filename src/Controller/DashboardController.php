ff8b353cdab4c702f0ec8e85c6a4d18a6679d3ae
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-02 19:58:23 -0300

    Fix: remove Cache dependency from DashboardController (AdapterInterface não existe como serviço)
    - Remove argumento $cache do construtor
    - Mantem apenas EntityManagerInterface e WazeTvtRouteExecutionRepository
    - Query já é simples e não precisa de cache nesta camada

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,13 +8,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class DashboardController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly AdapterInterface $cache,
         private readonly WazeTvtRouteExecutionRepository $routeExecutionRepository
     ) {
     }
