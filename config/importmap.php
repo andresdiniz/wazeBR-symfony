5200835643ac128a154128361e03b44bc3b90108
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-06 12:24:28 -0300

    Update importmap.php

--- config/importmap.php
+++ config/importmap.php
@@ -1,8 +1,5 @@
 return [
-    'app' => [
-        'path' => 'assets/app.js',
-        'entrypoint' => true,
-    ],
+
     'chart.js' => [
         'version' => '4.5.1',
         'entrypoint' => true,
@@ -12,11 +9,6 @@ return [
     ],
     'reset_password' => [
         'path' => 'assets/controllers/reset_password.js',
-        'entrypoint' => true,
-    ],
-    // Certifique-se que o caminho está correto
-    'admin' => [
-        'path' => 'assets/js/admin.js',
         'entrypoint' => true,
     ],
 ];
