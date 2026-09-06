5996f11504fb989c0737ac207399ea1e40c846ed
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-06 17:43:16 -0300

    Update importmap.php

--- config/importmap.php
+++ config/importmap.php
@@ -1,22 +1,13 @@
 <?php
 
+/**
+ * Returns the importmap for this application.
+ *
+ * @return array<string, array{path?: string, version?: string, entrypoint?: bool}>
+ */
 return [
     'app' => [
         'path' => 'assets/app.js',
-        'entrypoint' => true,
-    ],
-
-    'chart.js' => [
-        'version' => '4.5.1',
-        'entrypoint' => true,
-    ],
-
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
-
-    'reset_password' => [
-        'path' => 'assets/controllers/reset_password.js',
         'entrypoint' => true,
     ],
 ];
