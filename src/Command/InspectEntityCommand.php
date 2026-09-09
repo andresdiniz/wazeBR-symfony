7c98ad39394cc64433c765a9fcc64556707e1dfe
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-09 18:51:07 -0300

    Update debug:inspect-entity command to accept entity name argument
    - Now accepts entity name as argument (Partner, WazeFeed, etc.)
    - Shows properties and getter methods
    - Helps discover correct method names for entities

--- src/Command/InspectEntityCommand.php
+++ src/Command/InspectEntityCommand.php
@@ -4,41 +4,50 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Entity\WazeFeedCollection;
+use App\Entity\Partner;
+use App\Entity\WazeFeed;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'debug:inspect-entity',
-    description: 'Inspects WazeFeedCollection entity methods',
+    description: 'Inspects entity methods',
 )]
 class InspectEntityCommand extends Command
 {
+    protected function configure(): void
+    {
+        $this->addArgument('entity', InputArgument::REQUIRED, 'Entity class name (e.g., Partner, WazeFeed)');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-
-        $reflection = new \ReflectionClass(WazeFeedCollection::class);
+        $entityName = $input->getArgument('entity');
         
-        $io->title('WazeFeedCollection Entity');
+        $className = match($entityName) {
+            'Partner' => Partner::class,
+            'WazeFeed' => WazeFeed::class,
+            default => throw new \InvalidArgumentException("Unknown entity: $entityName"),
+        };
+
+        $reflection = new \ReflectionClass($className);
+        
+        $io->title(sprintf('%s Entity', $entityName));
         
         $io->section('Properties');
         foreach ($reflection->getProperties() as $property) {
-            $io->text(sprintf('- %s (%s)', $property->getName(), $property->getType() ?? 'mixed'));
+            $io->text(sprintf('- %s', $property->getName()));
         }
         
-        $io->section('Methods (setters)');
+        $io->section('Methods (getters)');
         foreach ($reflection->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
-            if (str_starts_with($method->getName(), 'set')) {
-                $params = [];
-                foreach ($method->getParameters() as $param) {
-                    $type = $param->getType() ? $param->getType()->getName() : 'mixed';
-                    $params[] = sprintf('%s: %s', $param->getName(), $type);
-                }
-                $io->text(sprintf('- %s(%s)', $method->getName(), implode(', ', $params)));
+            if (str_starts_with($method->getName(), 'get') || str_starts_with($method->getName(), 'is')) {
+                $io->text(sprintf('- %s()', $method->getName()));
             }
         }
 
