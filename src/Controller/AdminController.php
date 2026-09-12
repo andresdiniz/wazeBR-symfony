3748c93d9fce69910b130fca739ae10af0fad7ee
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-12 16:27:01 -0300

    feat: add AdminController with user index route

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin/users', name: 'admin_user_index', methods: ['GET'])]
+    public function users(UserRepository $userRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
