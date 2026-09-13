c290991fbc552d207100064c6d01ccad22c4106c
Andre Soares Diniz <69819014+andresdiniz@users.noreply.github.com>
2026-09-12 23:43:31 -0300

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -4,5 +4,9 @@ return [
     'app' => [
         'path' => './assets/js/app.js',
         'entrypoint' => true,
-    ]
+    ],
+    'dashboard' => [
+        'path' => './assets/js/dashboard.js',
+        'entrypoint' => true,
+    ],
 ];
